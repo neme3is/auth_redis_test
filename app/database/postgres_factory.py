0d80fdf1995419b232a13ac36c88988fcd392981
Aleksandr I <ivalex70@gmail.com>
2025-03-31 21:10:52 +0300

    refactoring

--- app/database/postgres_factory.py
+++ app/database/postgres_factory.py
@@ -3,21 +3,37 @@ from contextlib import asynccontextmanager
 from app.config import settings
 
 
-DATABASE_URL = (f"postgresql+asyncpg://{settings.postgres_settings.db_user}:{settings.postgres_settings.db_password}"
+class SqlSessionManager:
+    _engine = None
+    _session_factory  = None
+
+    @classmethod
+    def init_session(cls):
+        if cls._engine is None:
+            db_url = (
+                f"postgresql+asyncpg://{settings.postgres_settings.db_user}:{settings.postgres_settings.db_password}"
                 f"@{settings.postgres_settings.db_host}/{settings.postgres_settings.db_name}")
+            cls._engine = create_async_engine(db_url, echo=True)
+            cls._session_factory  = async_sessionmaker(bind=cls._engine, class_=AsyncSession, expire_on_commit=False)
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+    @classmethod
+    @asynccontextmanager
+    async def get_session(cls) -> AsyncSession:
+        if cls._session_factory  is None:
+            raise RuntimeError("DatabaseSessionManager не был инициализирован.")
 
-async_session_factory = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+        async with cls._session_factory() as session:
+            try:
+                yield session
+            except Exception:
+                await session.rollback()
+                raise
+            finally:
+                await session.close()
 
-
-@asynccontextmanager
-async def get_session():
-    async with async_session_factory() as session:
-        try:
-            yield session
-        except Exception:
-            await session.rollback()
-            raise
-        finally:
-            await session.close()
+    @classmethod
+    async def close(cls):
+        if cls._engine:
+            await cls._engine.dispose()
+            cls._engine = None
+            cls._session_factory = None
--- app/main.py
+++ app/main.py
@@ -1,10 +1,24 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
+from app.database.postgres_factory import SqlSessionManager
 from app.routes.auth import router
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    SqlSessionManager.init_session()
+    print("✅ Database initialized")
+
+    yield  # Переход к работе сервиса
+
+    await SqlSessionManager.close()
+    print("❌ Database connection closed")
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(router)
 
 
